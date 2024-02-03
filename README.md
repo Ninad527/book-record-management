a90d0a945650d403f318fa746773fe150dbdb4b0
Ninad527 <ninadmungekar180@gmail.com>
2024-02-03 12:53:46 +0530

    Added new Routes

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # book-record-management
-Creating a book record managment API Backend for the management of records and books 
+
+Creating a book record managment API Backend for the management of records and books
+
+# Routes and Endpoints
+
+## /users
+
+POST: Create a new users
+GET: Get all list of users ✔
+
+## /users/{id}
+
+GET: Get a user by ID
+PUT: Update a user by ID
+DELETE: Delete a user by ID (check if he/she still has an issued book) (is there any fine to be paid)
+
+## /users/subscriptions-details/{id}
+
+GET: Get users subscription details
+
+1. Date of Subscription
+2. Valid till
+3. Fine iif any
+
+## /books
+
+GET: Get all books
+POST: Create/add a new book
+
+## /books/{id}
+
+GET: Get a book by ID
+PUT: update a book by ID
+
+## /books/issued
+
+GET: Get all issued books
+
+## /books/issued/withFine
+
+GET: Get all issued books with fine
+
+# Subscription Types
+
+1. Basic (3 months)
+2. Standard (6 months)
+3. Premium (12 months)
+
+if the subscription date is 01/08/22
+and subscription type is Standard
+the valid till date will be 01/02/23
+
+If he has an isuued book and the issued book is to be returned at 01/01/23
+If he missed the date of return, the he gets a fine of Rs. 100./
+
+If he has n isuued book and the issued book is to be returned at 01/01/23
+If he missed the date of return, and his subscription also expires, the he will get a fine of Rs 200./
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {users} = require("./data/users.json");
 
 const PORT = 8081;
 
@@ -10,6 +11,107 @@ app.get("/", (req, res) =>{
     res.status(200).json ({
         message : "Server is Up and running"
     });  
+});
+
+/**
+ * Route: /users
+ * Method: GET
+ * Description: Get all users
+ * Access: Public
+ * Parameters: None
+ */
+app.get("/users", (req, res)=>{
+    res.status(200).json({
+        success: true,
+        data: users,
+    })
+})
+
+/**
+ * Route: /users/:id
+ * Method: GET
+ * Description: Get users by ID
+ * Access: Public
+ * Parameters: id
+ */
+ app.get("/users/:id", (req, res)=> {
+    const{id} = req.params;
+    const user = users.find((each) => each.id === id);
+
+    if(!user){
+        return res.status(404).json({
+            success: false,
+            message: "user not found"
+        });
+    }
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+ });
+
+ /**
+ * Route: /users
+ * Method: Post
+ * Description: Create new user
+ * Access: Public
+ * Parameters: none
+ */
+app.post("/users", (req, res) =>{
+    const{id, name, surname, email, subscriptionType, subscriptionDate} = req.body;
+    const user = users.find((each) => each.id === id);
+
+    if(user){
+        return res.status(404).json({
+        success: false,
+        message: "User exist with this ID"
+        });
+    }
+
+    users.push({
+        id,
+        name,
+        surname,
+        email, 
+        subscriptionType,
+        subscriptionDate
+    });
+    res.status(200).json({
+        success: true,
+        data: users,
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: Put
+ * Description: Update a user
+ * Access: Public
+ * Parameters: id
+ */
+app.put("/users/:id", (req, res) => {
+    const{id} = req.params;
+    const{data} = req.body;
+    const user = users.find((each) => each.id === id);
+    if(!user){
+        return res.status(404).json({
+            success: false,
+            message: "User Not Found with this ID"
+        });
+    }
+    const updateduser = users.map((each) =>{
+        if(each.id === id){
+            return{
+                ...each,
+                ...data
+            };
+        }
+        return each;
+    });
+    return res.status(200).json({
+        success: true,
+        data: updateduser
+    });
 });
 
 app.get("*", (req, res)=>{
